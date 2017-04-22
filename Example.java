96ff80ee9bc0329e0ea3ebdf5813af0a2b27a3a3
root <root@ip-172-31-22-111.ec2.internal>
2017-04-22 01:36:27 +0000

    working example

--- Example.java
+++ Example.java
@@ -4,10 +4,14 @@ public class Example {
 	public static void main(String[] args) {
 		//java 8 lambda expression 
 		System.out.println("java 8 Lambda in action");
-		Double result1 = Calculator.calculate(30d, 70d, (a, b) -> a + b);
-		Double another = Calculator.calculate(4d,6d, (a,b) -> a*b); 
-			System.out.println(another) ; 
-        		System.out.println(result1); //100.0
+
+		System.out.println("********************************************");
+		Double addition = Calculator.calculate(30d, 70d, (a, b) -> a + b);
+		Double mult = Calculator.calculate(30d,70d, (a,b) -> a*b); 
+			System.out.println("java 8 addition");
+			System.out.println(addition) ; 
+			System.out.println("java 8 multiplication");
+			System.out.println(mult);
 
 	}
 
